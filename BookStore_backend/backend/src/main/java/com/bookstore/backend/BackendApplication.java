130dee8d689e635035670c0cd65d1820a23a548e
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-04-08 14:43:01 +0700

    fix: resolve lombok issues and update base entity configuration

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/BackendApplication.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/BackendApplication.java
@@ -3,10 +3,14 @@ package com.bookstore.backend;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan("com.bookstore.backend.entity")
+@EnableJpaRepositories("com.bookstore.backend.repository")
 public class BackendApplication {
 
 
