62226367c7c5e92e570267e0247a5378793fec07
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-04-10 09:42:31 +0700

    fect: complete registration logic

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/controller/AuthController.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/controller/AuthController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,31 +15,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 import com.bookstore.backend.dto.request.LoginRequest;
 import com.bookstore.backend.dto.response.JwtTokenResponse;
 import com.bookstore.backend.dto.response.UserProfileResponse;
 import com.bookstore.backend.service.AuthService;
+import com.bookstore.backend.dto.request.RegistrationRequest;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @Tag(name = "Auth")
-@SecurityRequirement(name = "bearerAuth")
+@RequiredArgsConstructor
+//@SecurityRequirement(name = "bearerAuth")
 public class AuthController {
 
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
+//    public AuthController(AuthService authService) {
+//        this.authService = authService;
+//    }
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegistrationRequest request) {
+        authService.register(request);
+        return ResponseEntity.ok("Đăng ký tài khoản thành công!");
     }
-
     @PostMapping("/login")
     public ResponseEntity<JwtTokenResponse> login(@Valid @RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request.username(), request.password()));
     }
-
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping("/me")
     public ResponseEntity<UserProfileResponse> currentUser(Authentication authentication) {
-        List<String> roles = authentication.getAuthorities().stream()
+         List<String> roles = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .toList();
 
