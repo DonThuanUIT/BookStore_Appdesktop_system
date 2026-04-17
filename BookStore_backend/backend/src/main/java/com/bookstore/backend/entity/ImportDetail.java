46ac3c8589aa617df35ac9f51819dab36deccc8b
Huynh Gia Thinh <thinh871779@gmail.com>
2026-04-17 08:57:39 +0700

    fix: Book CRUD

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/entity/ImportDetail.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/entity/ImportDetail.java
@@ -1,12 +1,19 @@
 package com.bookstore.backend.entity;
 
+import com.bookstore.backend.listener.ImportDetailQuantityListener;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@EntityListeners(ImportDetailQuantityListener.class)
 @Table(name = "import_details")
+@Getter
+@Setter
 public class ImportDetail extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "import_id")
