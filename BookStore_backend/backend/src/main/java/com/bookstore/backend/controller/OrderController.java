ebe940373ed7581c5c5fbe2ad4c4660ffb77a197
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-05-09 10:21:43 +0700

    create order

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/controller/OrderController.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/controller/OrderController.java
@@ -1,15 +1,21 @@
 package com.bookstore.backend.controller;
 
+import com.bookstore.backend.dto.request.CreateOrderRequest;
 import com.bookstore.backend.dto.request.UpdateOrderStatusRequest;
 import com.bookstore.backend.dto.response.OrderResponse;
 import com.bookstore.backend.entity.Order;
+import com.bookstore.backend.entity.User;
 import com.bookstore.backend.service.OrderService;
+import com.bookstore.backend.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/orders")
@@ -17,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private UserService userService;
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
     public ResponseEntity<Page<OrderResponse>> getAllOrders(
@@ -32,5 +40,17 @@ public class OrderController {
             @PathVariable Long id,
             @RequestBody UpdateOrderStatusRequest request) {
         return ResponseEntity.ok(orderService.updateStatus(id, request.status()));
+    }
+    @PostMapping
+    @SecurityRequirement(name = "bearerAuth")
+    public ResponseEntity<OrderResponse> createOrder(
+            @RequestBody CreateOrderRequest request,
+            Authentication authentication) {
+
+        String username = authentication.getName();
+        User currentUser = userService.findByUsername(username);
+
+        // 2. Gọi service để tạo đơn hàng
+        return ResponseEntity.ok(orderService.createOrder(request, currentUser));
     }
 }
--- BookStore_backend/backend/src/main/java/com/bookstore/backend/service/OrderService.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/service/OrderService.java
@@ -1,9 +1,17 @@
 package com.bookstore.backend.service;
 
+import com.bookstore.backend.dto.request.CreateOrderRequest;
+import com.bookstore.backend.dto.request.OrderItemRequest;
 import com.bookstore.backend.dto.response.OrderResponse;
 import com.bookstore.backend.dto.response.UserProfileResponse;
+import com.bookstore.backend.entity.Book;
 import com.bookstore.backend.entity.Order;
+import com.bookstore.backend.entity.OrderDetail;
+import com.bookstore.backend.entity.User;
+import com.bookstore.backend.repository.BookRepository;
+import com.bookstore.backend.repository.OrderDetailRepository;
 import com.bookstore.backend.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,13 +19,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.stream.Collectors;
+
 
 @Service
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
 
     public Page<OrderResponse> getAllOrders(int page, int size, String sortBy, String direction) {
         Sort sort = direction.equalsIgnoreCase(Sort.Direction.ASC.name())
@@ -67,5 +81,39 @@ public class OrderService {
         }
         order.setStatus(newStatus.toUpperCase());
         return convertToResponse(orderRepository.save(order));
+    }
+    @Transactional
+    public OrderResponse createOrder(CreateOrderRequest request, User user) {
+        Order order = Order.builder()
+                .user(user)
+                .status("PENDING")
+                .totalAmount(BigDecimal.ZERO)
+                .discount(BigDecimal.ZERO)
+                .finalAmount(BigDecimal.ZERO)
+                .build();
+        Order savedOrder = orderRepository.save(order);
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (OrderItemRequest item : request.items()) {
+            Book book = bookRepository.findById(item.bookId())
+                    .orElseThrow(() -> new RuntimeException("No book found!"));
+
+            OrderDetail detail = OrderDetail.builder()
+                    .order(savedOrder)
+                    .book(book)
+                    .quantity(item.quantity())
+                    .price(book.getSellPrice())
+                    .build();
+
+            orderDetailRepository.save(detail);
+
+            BigDecimal itemTotal = book.getSellPrice().multiply(BigDecimal.valueOf(item.quantity()));
+            total = total.add(itemTotal);
+        }
+
+        savedOrder.setTotalAmount(total);
+        savedOrder.setFinalAmount(total);
+
+        return convertToResponse(orderRepository.save(savedOrder));
     }
 }
