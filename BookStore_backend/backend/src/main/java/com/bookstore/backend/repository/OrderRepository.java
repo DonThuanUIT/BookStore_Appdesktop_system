dc614fed6ae3a85ae97de192a2258baed01568ab
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-05-10 15:28:50 +0700

    feat: Complete the order history API and confirm receipt of goods

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/OrderRepository.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.bookstore.backend.repository;
 import com.bookstore.backend.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
+    @Query("SELECT o FROM Order o WHERE o.user.id = :userId")
+    Page<Order> findByUserId(@Param("userId") Long userId, Pageable pageable);
 }
