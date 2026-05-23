e2a6798e59911b54585ab9f7d56df8633651bd08
Huynh Gia Thinh <thinh871779@gmail.com>
2026-05-23 14:08:19 +0700

    feat: add get by name api for author and category

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/CategoryRepository.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Repository;
 import com.bookstore.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findFirstByNameIgnoreCase(String name);
 }
