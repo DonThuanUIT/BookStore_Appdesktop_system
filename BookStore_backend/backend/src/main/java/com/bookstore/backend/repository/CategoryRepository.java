efa6aef1ff36ab60b99d5440aaed86074781d247
Huynh Gia Thinh <thinh871779@gmail.com>
2026-05-25 10:28:34 +0700

    feat: Check for duplicate Author and Category Name

--- BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/CategoryRepository.java
+++ BookStore_backend/backend/src/main/java/com/bookstore/backend/repository/CategoryRepository.java
@@ -5,8 +5,10 @@ import com.bookstore.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByNameContainingIgnoreCaseOrderByNameAsc(String keyword);
+    Optional<Category> findFirstByNameIgnoreCaseOrderByIdAsc(String name);
 }
